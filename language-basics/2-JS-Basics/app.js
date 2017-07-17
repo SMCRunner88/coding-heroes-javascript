5816e7a7b7c2ac0a004fce9c27022fa84c8daa68
Scott Cook <smcrunner88@gmail.com>
2017-07-17 12:20:07 -0700

    If/else lesson

--- language-basics/2-JS-Basics/app.js
+++ language-basics/2-JS-Basics/app.js
@@ -29,10 +29,35 @@ job = 'driver';
 //var lastName = prompt('What is the last name?');
 //console.log(lastName);
 
-// Lecture: opererators
-var currentYear = 2016
-var birthYear = currentYear - 26;
+// Lecture: operators
+// var currentYear = 2016
+// var birthYear = currentYear - 26;
 
-birthYear = currentYear - 26 * 2;
+// birthYear = currentYear - 26 * 2;
 
-console.log(birthYear);
+// console.log(birthYear);
+
+// Lecture: if/else statements
+var name = 'John';
+var age = 26;
+var isMarried = 'no';
+
+// if (isMarried === 'yes') {
+// 	console.log(name + ' is married!');
+// } else {
+// 	console.log(name + ' will hopefully marry soon')
+// }
+
+isMarried = false
+
+if (isMarried) {
+	console.log('YES!')
+} else {
+	console.log('NO!')
+}
+
+/*
+Two equal signs does type coercion
+Triple does not
+*/
+
